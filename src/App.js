c74f92421952579ef79ce80182228a23cd42aa14
Abhijeet <abhi414@gmail.com>
2024-02-27 16:48:35 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Router>
         <div>
           <Routes>
-            <Route exact path="/food-master" element={<Home />} />
+            <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
             <Route exact path="/createuser" element={<Signup />} />
             <Route exact path="/myOrder" element={<MyOrder />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
         <div className="container-fluid">
           <Link
             className="navbar-brand fs-2 text-light text-bold fw-bold "
-            to="/food-master"
+            to="/"
           >
             FOODIE
           </Link>
@@ -40,7 +40,7 @@ export default function Navbar() {
                 <Link
                   className="nav-link  active fs-5"
                   aria-current="page"
-                  to="/food-master"
+                  to="/"
                 >
                   Home
                 </Link>
